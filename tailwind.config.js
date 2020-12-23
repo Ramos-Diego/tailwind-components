952010a26b693880d4eca3c88294951163cbdb29
Ramos-Diego <RamosDiego@pm.me>
2020-12-22 22:41:55 -0500

    logo, card and modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
+      boxShadow: {
+        // prettier-ignore
+        'md-invert':'0 4px 6px -1px rgba(255, 255, 255, 0.1), 0 2px 4px -1px rgba(255, 255, 255, 0.06)',
+      },
       colors: {
         ...colors,
         gray: colors.trueGray,
@@ -12,6 +16,10 @@ module.exports = {
       },
     },
   },
-  variants: {},
+  variants: {
+    // Control the shadows on dark mode to invert colors
+    // prettier-ignore
+    boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'dark'],
+  },
   plugins: [],
 }
