23f09e3e41f54004cd38fe1c1ea380df9aaa34c9
Ramos-Diego <RamosDiego@pm.me>
2020-12-29 00:02:39 -0500

    simpler inputs, fixed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,20 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   darkMode: 'media',
   theme: {
-    extend: {
-      colors: {
-        ...colors,
-        gray: colors.trueGray,
-      },
+    // https://tailwindcss.com/docs/customizing-colors
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.warmGray, // Change grays
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
     },
   },
   variants: {},
